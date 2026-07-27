c82028a531517f05b52d15880e2a1c9c9d547894
FaizulDevSkill2025 <csldotnet@gmail.com>
2026-07-27 12:48:53 +0600

    Project Updated

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace IINVT.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string Code { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/Supplier.cs
@@ -0,0 +1,12 @@
+﻿namespace IINVT.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
