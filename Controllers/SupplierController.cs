3ef41bdba31b3780ddcc8dc07af87a646d2b6f1c
FaizulDevSkill2025 <csldotnet@gmail.com>
2026-07-30 23:02:32 +0600

    Supplier Created

---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,96 @@
+﻿using IINVT.Interfaces;
+using IINVT.Models;
+using IINVT.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IINVT.Controllers
+{
+    public class SupplierController : BaseController<Supplier>
+    {
+        protected readonly GenericRepository<Supplier> _customerRepository;
+
+        public SupplierController(IGeneric<Supplier> repository) : base(repository)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            var supplier = _repository.GetAll();
+            return View(supplier);
+        }
+
+        [HttpGet("{id}")]
+        public new Supplier GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Supplier supplier)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _repository.Add(supplier);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var supplier = _repository.GetById(id);
+            return View(supplier);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Supplier supplier)
+        {
+
+            if (ModelState.IsValid)
+            {
+               _repository.Update(supplier);
+               return RedirectToAction(nameof(Index));
+            }
+
+            return View(supplier);
+        }
+
+        [HttpPut]
+        public new void Update(Supplier supplier)
+        {
+            _repository.Update(supplier);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var supplier = _repository.GetById(id);
+            return View(supplier);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var supplier = _repository.GetById(id);
+            return View(supplier);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var supplier = _repository.GetById(id);
+            _repository.Delete(supplier);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -2,7 +2,7 @@
 {
     public class Supplier
     {
-        public int Id { get; set; }
+        public int SupplierID { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
